a46eaccb4ae80fae5aeb5f2fdbc43ad27b02a217
jasminesya30 <jasminesya30@gmail.com>
2024-06-06 17:38:57 +0900

    A

--- src/main/java/com/example/tabelog/TabelogKadaiApplication.java
+++ src/main/java/com/example/tabelog/TabelogKadaiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootTabelogKadaiApplication {
+public class TabelogKadaiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootTabelogKadaiApplication.class, args);
+		SpringApplication.run(TabelogKadaiApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/tabelog/TabelogKadaiApplicationTests.java
+++ src/test/java/com/example/tabelog/TabelogKadaiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootTabelogKadaiApplicationTests {
+class TabelogKadaiApplicationTests {
 
 	@Test
 	void contextLoads() {
