198ebe17c2860886cc077d7b6f28dfbe86ded67c
jasminesya30 <jasminesya30@gmail.com>
2024-07-02 07:24:12 +0900

    E

--- src/main/resources/static/js/flatpickr.js
+++ src/main/resources/static/js/flatpickr.js
@@ -1,7 +1,7 @@
 let maxDate = new Date();
 maxDate.setMonth(maxDate.getMonth() + 3);
 
-flatpickr('#CheckinDate', {
+flatpickr('#reservationDateTime', {
 	enableTime: true, // 時間の選択を有効にする
 	enableSeconds: false, // 秒の選択を無効にする
 	time_24hr: true, // 24時間表示にする（任意の設定）
